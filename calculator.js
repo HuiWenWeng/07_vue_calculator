b04e3b7950036ace4e4499e050cca9df6cd2ca98
Hui Wen Weng <hweng40@stuy.edu>
2024-02-29 14:20:33 -0500

    setup

--- calculator.js
+++ calculator.js
@@ -1,1 +1,27 @@
+import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+createApp({ 
+    data() {
+    return { 
+        operand: "",
+        result: "",
+        part1_value: 0,
+        part2_value: 0,
+    }; 
+},
 
+methods: { 
+    calculate() {
+        // this.operand = event.target.options[event.target.options.selectedIndex].value;
+        if (this.operand == "+") {
+            this.result = this.part1_value + this.part2_value;
+        } else if (this.operand == "-") {
+            this.result = this.part1_value - this.part2_value;
+        } else if (this.operand == "*") {
+            this.result = this.part1_value * this.part2_value;
+        } else if (this.operand == "/") {
+            this.result = this.part1_value / this.part2_value;
+        }
+    },
+} 
+
+}).mount('#app')
