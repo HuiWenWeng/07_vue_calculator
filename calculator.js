4ea43396be1b845bed05123384e4f7ccba51664f
Hui Wen Weng <hweng40@stuy.edu>
2024-03-04 14:18:00 -0500

    merge conflicts

--- calculator.js
+++ calculator.js
@@ -4,6 +4,7 @@ import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
         return { 
             input: "",
             result: "",
+            history: [],
         }; 
     },
 
@@ -15,8 +16,15 @@ import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
             this.input = this.input.substring(0, this.input.length - 1);
         },
         calculate() {
-            return "stub";
-        }
+            this.result = this.input;
+            this.history.push(this.result)
+            // make sure that the expression is valid (no letters, no number after sqrt)
+            // squared --> must display num^2 instead of num x^2
+            // % --> divide number by 100
+            // modulo --> convert to % before eval
+            // recursively check for ()
+            // append the result to history, which displays all the computations done
+        },
     } 
 
 }).mount('#app')
