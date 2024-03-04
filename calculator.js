a10e3caa5bcc08bc7ecda1b14579e1d6fa1e4b44
ty-c1016 <tylerchan586@gmail.com>
2024-03-04 14:23:15 -0500

    calculate is fully functional WOOOO

--- calculator.js
+++ calculator.js
@@ -5,7 +5,7 @@ import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
             input: "",
             copyInput: "",
             result: "",
-            answer: "",
+            answer: 0,
             openParenTracker: 0,
             closeParenTracker: 0,
             reverseStr: ""
@@ -20,15 +20,20 @@ import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
             this.input = this.input.substring(0, this.input.length - 1);
         },
         calculate() {
-            this.copyInput = this.input;
-            if (this.input.includes("(")) {
-                this.openParenTracker = this.input.indexOf("(")
-                this.reverseString(this.input)
-                this.closeParenTracker = this.input.indexOf(")")
-                console.log(this.copyInput)
-                this.copyInput.replace(this.input.substring(this.openParenTracker, this.closeParenTracker + 1), eval(this.input.substring(this.openParenTracker + 1, this.closeParenTracker)).toString())
-                console.log(this.copyInput)
-            }
+            // this.copyInput = this.input;
+            // while (this.copyInput.includes("(")) {
+            //     this.openParenTracker = this.input.indexOf("(")
+            //     this.reverseString(this.input)
+            //     this.closeParenTracker = this.input.indexOf(")")
+            //     console.log(this.copyInput)
+            //     console.log(this.openParenTracker)
+            //     console.log(this.closeParenTracker)
+            //     const replacement = (eval(this.input.substring(this.openParenTracker + 1, this.closeParenTracker)))
+            //     this.copyInput = this.copyInput.replace((this.input.substring(this.openParenTracker, this.closeParenTracker + 1)), replacement)
+            //     console.log(this.copyInput)
+            // }
+            this.answer = eval(this.input)
+            console.log(this.answer)
         },
         reverseString(str) { 
             this.reverseStr +=  str.split('').reverse().join(''); 
