6ae6873c6837d8148f2437b1d2791cdc0cd98432
ty-c1016 <tylerchan586@gmail.com>
2024-03-04 10:15:27 -0500

    aworking on calculate

--- calculator.js
+++ calculator.js
@@ -3,7 +3,12 @@ import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
         data() {
         return { 
             input: "",
+            copyInput: "",
             result: "",
+            answer: "",
+            openParenTracker: 0,
+            closeParenTracker: 0,
+            reverseStr: ""
         }; 
     },
 
@@ -15,7 +20,18 @@ import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
             this.input = this.input.substring(0, this.input.length - 1);
         },
         calculate() {
-            return "stub";
+            this.copyInput = this.input;
+            if (this.input.includes("(")) {
+                this.openParenTracker = this.input.indexOf("(")
+                this.reverseString(this.input)
+                this.closeParenTracker = this.input.indexOf(")")
+                console.log(this.copyInput)
+                this.copyInput.replace(this.input.substring(this.openParenTracker, this.closeParenTracker + 1), eval(this.input.substring(this.openParenTracker + 1, this.closeParenTracker)).toString())
+                console.log(this.copyInput)
+            }
+        },
+        reverseString(str) { 
+            this.reverseStr +=  str.split('').reverse().join(''); 
         }
     } 
 
