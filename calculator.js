9a7fb3821a69cd0374593a55a521a76d590cd7a1
ty-c1016 <tylerchan586@gmail.com>
2024-03-04 14:39:18 -0500

    switch the operation symbols for accuracy

--- calculator.js
+++ calculator.js
@@ -6,9 +6,7 @@ import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
             copyInput: "",
             result: "",
             answer: "",
-            openParenTracker: 0,
-            closeParenTracker: 0,
-            reverseStr: ""
+            history: [],
         }; 
     },
 
@@ -20,19 +18,18 @@ import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
             this.input = this.input.substring(0, this.input.length - 1);
         },
         calculate() {
+            //changes the operations by making a copy
             this.copyInput = this.input;
-            // while (this.copyInput.includes("(")) {
-            //     this.openParenTracker = this.input.indexOf("(")
-            //     this.reverseString(this.input)
-            //     this.closeParenTracker = this.input.indexOf(")")
-            //     console.log(this.copyInput)
-            //     console.log(this.openParenTracker)
-            //     console.log(this.closeParenTracker)
-            //     const replacement = (eval(this.input.substring(this.openParenTracker + 1, this.closeParenTracker)))
-            //     this.copyInput = this.copyInput.replace((this.input.substring(this.openParenTracker, this.closeParenTracker + 1)), replacement)
-            //     console.log(this.copyInput)
-            // }
-            this.answer = eval(this.input)
+            if (this.copyInput.includes("x")) {
+                this.copyInput.replace(/x/g, "*")
+            }
+            if (this.copyInput.includes ("÷")) {
+                this.copyInput.replace(/÷/g, "/")
+            }
+            if (this.copyInput.includes ("mod")) {
+                this.copyInput.replace(/mod/g, "%")
+            }
+            this.answer = eval(this.copyInput)
             console.log(this.answer)
         },
         reverseString(str) { 
