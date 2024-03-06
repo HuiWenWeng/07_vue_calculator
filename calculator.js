ba73a9047148138d9a7de5f556e4eaa235867530
ty-c1016 <tylerchan586@gmail.com>
2024-03-05 23:41:30 -0500

    edge cases for when multplication appears next to parentheses or sqrt

--- calculator.js
+++ calculator.js
@@ -10,6 +10,7 @@ import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
             copyHistory: [],
             inputValid: true,
             undoTracker: 0,
+            edgeCase: [],
         }; 
     },
 
@@ -42,6 +43,36 @@ import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
                 this.copyInput = this.copyInput.replaceAll(/²/g, "**2");
             }
 
+            //the edge cases where a number appears before a parentheses (it's multiplication)
+            if (this.copyInput.includes("(")) {
+                this.edgeCase = []
+                for (var i=0; i<this.input.length; i++) {
+                    if (this.input.charAt(i) == "(") {
+                        this.edgeCase.push(i)
+                    }
+                }
+                console.log(this.edgeCase)
+                for (var i=0; i<this.edgeCase.length; i++) {
+                    if (this.input[this.edgeCase[i] - 1]>='0' && this.input[this.edgeCase[i] - 1]<='9') {
+                        this.copyInput = this.copyInput.replaceAll("(", "*(");
+                    }
+                }
+            }
+            if (this.copyInput.includes(")")) {
+                this.edgeCase = []
+                for (var i=0; i<this.input.length; i++) {
+                    if (this.input.charAt(i) == ")") {
+                        this.edgeCase.push(i)
+                    }
+                }
+                console.log(this.edgeCase)
+                for (var i=0; i<this.edgeCase.length; i++) {
+                    if (this.input[this.edgeCase[i] + 1]>='0' && this.input[this.edgeCase[i] - 1]<='9') {
+                        this.copyInput = this.copyInput.replaceAll(")", ")*");
+                    }
+                }
+            }
+
             // checks to make sure there are no letters in the input
             if (/[a-zA-Z]/g.test(this.copyInput)) {
                 alert("Error! Unknown variable detected.");
@@ -50,6 +81,18 @@ import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
             //you need to check square root last for our sakes
             if (this.copyInput.includes("√")) {
+                this.edgeCase = []
+                for (var i=0; i<this.input.length; i++) {
+                    if (this.input.charAt(i) == "√") {
+                        this.edgeCase.push(i)
+                    }
+                }
+                console.log(this.edgeCase)
+                for (var i=0; i<this.edgeCase.length; i++) {
+                    if (this.input[this.edgeCase[i] - 1]>='0' && this.input[this.edgeCase[i] - 1]<='9') {
+                        this.copyInput = this.copyInput.replaceAll(/√/g, "*Math.sqrt");
+                    }
+                }
                 this.copyInput = this.copyInput.replaceAll(/√/g, "Math.sqrt");
             }
 
